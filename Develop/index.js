afcb80fd7ab2cb491e23aa8161e8a9d744455736
ChucktownTiger <clemsonalum07@gmail.com>
2022-08-07 11:57:20 -0400

    initial code

---
+++ Develop/index.js
@@ -0,0 +1,19 @@
+// TODO: Include packages needed for this application
+import inquirer from "inquirer";
+import fs from 'fs'
+import util from "util"
+import generateMarkdown from "./utils/generateMarkdown.js"
+const writeFile= util.promisify(fs.writeFile);
+const nodeReadME = "./dist/README.md"
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
